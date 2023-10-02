cbfb77719bf000e632adb3f13c49171f51f79788
Jim Toogood <jimtoogood04@gmail.com>
2023-10-02 14:09:54 +0100

    1

--- Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
+++ Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
@@ -1,23 +1,7 @@
-// 101-HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
 
-//This is known as a "header file" - we will look at this later
-#include <stdio.h>
-
-//This is the entry point to our code - known as the "main function" - we will meet functions later
 int main()
 {
-    //Write Hello World to the terminal with puts  ("put string")
-    puts("Hello World");
-
-    /*
-        TASK: Add two more lines of code to the above so that the output reads as follows:
-
-        ***********
-        Hello World
-        ***********
-        
-    */ 
+    char name[] = "James Toogood";
+    printf("\nThe string '%s' contains %zd characters.\n\n", name, sizeof(name));
 }
-
-
